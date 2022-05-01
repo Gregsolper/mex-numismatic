6987db0a9360ee0eefd3fbfb68d2993d20d9d27e
Gregsolper <57849729+Gregsolper@users.noreply.github.com>
2022-05-01 18:05:03 +0200

    Campos adicionales

--- src/main/java/com/gregorio/numismatic/model/Coin.java
+++ src/main/java/com/gregorio/numismatic/model/Coin.java
@@ -19,20 +19,52 @@ public class Coin {
 	private String material;
 	private float diametro;
 	private float onzas;
+	private String coleccion;
+	private String nombre;
+	private String acabado;
+	private String ley;
+	private String anverso;
+	private String reverso;
+	private String comentario;
+	
 	
 	
 	
 	public Coin() {
 		super();
 	}
-	
-	public Coin(String valorFacial, String material, float diametro, float onzas) {
+
+	/**
+	 * @param id
+	 * @param valorFacial
+	 * @param material
+	 * @param diametro
+	 * @param onzas
+	 * @param colecion
+	 * @param nombre
+	 * @param acabado
+	 * @param ley
+	 * @param anverso
+	 * @param reverso
+	 * @param comentario
+	 */
+	public Coin(Long id, String valorFacial, String material, float diametro, float onzas, String coleccion,
+			String nombre, String acabado, String ley, String anverso, String reverso, String comentario) {
 		super();
+		this.id = id;
 		this.valorFacial = valorFacial;
 		this.material = material;
 		this.diametro = diametro;
 		this.onzas = onzas;
+		this.coleccion = coleccion;
+		this.nombre = nombre;
+		this.acabado = acabado;
+		this.ley = ley;
+		this.anverso = anverso;
+		this.reverso = reverso;
+		this.comentario = comentario;
 	}
+
 	public Long getId() {
 		return id;
 	}
@@ -64,7 +96,89 @@ public class Coin {
 		this.onzas = onzas;
 	}
 	
-	
+
+	public String getColeccion() {
+		return coleccion;
+	}
+
+
+
+	public void setColeccion(String coleccion) {
+		this.coleccion = coleccion;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getAcabado() {
+		return acabado;
+	}
+
+
+
+	public void setAcabado(String acabado) {
+		this.acabado = acabado;
+	}
+
+
+
+	public String getLey() {
+		return ley;
+	}
+
+
+
+	public void setLey(String ley) {
+		this.ley = ley;
+	}
+
+
+
+	public String getAnverso() {
+		return anverso;
+	}
+
+
+
+	public void setAnverso(String anverso) {
+		this.anverso = anverso;
+	}
+
+
+
+	public String getReverso() {
+		return reverso;
+	}
+
+
+
+	public void setReverso(String reverso) {
+		this.reverso = reverso;
+	}
+
+
+
+	public String getComentario() {
+		return comentario;
+	}
+
+
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
 	
 	
 
---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into coin (id,valorfacial,material,diametro,onzas,coleccion,nombre,ley,anverso,reverso,comentario) values
+(1,"1 onza","ORO",34.5,1,"Serie Libertad","libertad","Satín","0.999","https://www.banxico.org.mx/multimedia/libertadOroFrente.png",
+"https://www.banxico.org.mx/multimedia/LibertadOroVuelta.png",
+"En 1981 se determinó la acuñación de piezas con contenido de oro puro de 1, 1/2 y 1/4 de onza, y se fijó como anverso común el Escudo Nacional. En el reverso presentan el mismo diseño del Centenario, una Victoria Alada, representada en el monumento del Ángel de la Independencia, y como fondo los volcanes Popocatépetl e Iztaccíhuatl.");
