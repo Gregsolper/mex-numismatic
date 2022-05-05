12b0cf3d7a6d977afab958476c0cf36c818484a8
Gregsolper <57849729+Gregsolper@users.noreply.github.com>
2022-05-05 20:30:04 +0200

    nva version para Heroku

--- src/main/java/com/gregorio/numismatic/controller/coinController.java
+++ src/main/java/com/gregorio/numismatic/controller/coinController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.gregorio.numismatic.exception.ResourceNotFoundException;
 import com.gregorio.numismatic.model.Coin;
 import com.gregorio.numismatic.repository.CoinRepository;
 
+@CrossOrigin(origins= {"http://localhost:4200","https//localhost:8080"})
 @RestController
 @RequestMapping ("/api/")
 public class coinController {
